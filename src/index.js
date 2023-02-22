b59c5993a8a47d2ba375fdeaa2ed3860db383567
Ashnaali3255 <ashnaalipk@gmail.com>
2023-02-22 17:03:36 +0500

    Update files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import './style.css';
 
   // Lodash, now imported by this script
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+   element.classList.add('hello');
+   
    return element;
  }
 
